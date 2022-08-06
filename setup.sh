9df4b757c1b41f88922f9f30f4ed70985800666e
Luis Mayta <luis@hadenlabs.com>
2022-08-06 16:34:20 -0300

    chore: implement functions setup nvchad (#54)

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ YELLOW="\033[0;33m"
 BLUE="\033[0;36m"
 NORMAL="\033[0m"
 
-VIM_PLUG_URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 NVIM_MESSAGE_BREW="Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
 NVIM_MESSAGE_DONE="Keep calm and use nvim"
 NVIM_REPO_HTTPS="https://github.com/luismayta/nvimrc.git"
@@ -14,125 +13,136 @@ NVIM_ROOT_PATH="${HOME}/.nvimrc"
 NVIM_CONFIG_DIR="${HOME}/.config"
 NVIM_PACKAGE_NAME="nvim"
 PYTHON_PACKAGES_NVIM=(
-    msgpack
-    pynvim
-    jedi
+  msgpack
+  pynvim
+  jedi
 )
 NVIM_DST_NVIM_DIR="${NVIM_CONFIG_DIR}/nvim"
+NVIM_DST_LUA_DIR="${NVIM_DST_NVIM_DIR}/lua"
+NVIM_DST_CUSTOM_DIR="${NVIM_DST_LUA_DIR}/custom"
+NVIM_ROOT_LUA_PATH="${NVIM_ROOT_PATH}/nvim/lua"
+NVIM_ROOT_CUSTOM_PATH="${NVIM_ROOT_LUA_PATH}/custom"
 
 message_error() {
-    printf "${RED}%s${NORMAL}\n" "[ERROR]: ${1}"
-    return 0
+  printf "${RED}%s${NORMAL}\n" "[ERROR]: ${1}"
+  return 0
 }
 
 message_info() {
-    printf "${BLUE}%s${NORMAL}\n" "[INFO]: ${1}"
+  printf "${BLUE}%s${NORMAL}\n" "[INFO]: ${1}"
 }
 
 message_warning() {
-    printf "${YELLOW}%s${NORMAL}\n" "[WARNING]: ${1}"
+  printf "${YELLOW}%s${NORMAL}\n" "[WARNING]: ${1}"
 }
 
 message_success() {
-    printf "${GREEN}%s${NORMAL}\n" "🍺️ [SUCCESS]: ${1}"
-}
-
-nvim::install::vimplug() {
-    if ! type -p curl > /dev/null; then
-        message_warning "Please Install curl"
-        return
-    fi
-    message_info "Installing vim plug for ${NVIM_PACKAGE_NAME}"
-    # Install vim-plug
-    mkdir -p "${NVIM_ROOT_PATH}"/nvim/autoload
-    curl -Lo "${NVIM_ROOT_PATH}"/nvim/autoload/plug.vim "${VIM_PLUG_URL}"
-    message_success "Installed vim plug for ${NVIM_PACKAGE_NAME}"
+  printf "${GREEN}%s${NORMAL}\n" "🍺️ [SUCCESS]: ${1}"
 }
 
 nvim::install::dependences() {
-    message_info "Installing dependences ${NVIM_PACKAGE_NAME}"
-    mkdir -p "${NVIM_CONFIG_DIR}"
+  message_info "Installing dependences ${NVIM_PACKAGE_NAME}"
+  mkdir -p "${NVIM_CONFIG_DIR}"
 
-    # Install neovim-python; vim-plug requires neovim-python
-    if type -p pip > /dev/null; then
-        pip install --upgrade pip
-        for module in "${PYTHON_PACKAGES_NVIM[@]}"; do
-            pip install --user "${module}"
-        done
-    fi
+  # Install neovim-python; vim-plug requires neovim-python
+  if type -p pip > /dev/null; then
+      pip install --upgrade pip
+      for module in "${PYTHON_PACKAGES_NVIM[@]}"; do
+          pip install --user "${module}"
+      done
+  fi
 
-    if type -p pip3 > /dev/null; then
-        for module in "${PYTHON_PACKAGES_NVIM[@]}"; do
-            pip3 install --user "${module}"
-        done
-    fi
+  if type -p pip3 > /dev/null; then
+      for module in "${PYTHON_PACKAGES_NVIM[@]}"; do
+          pip3 install --user "${module}"
+      done
+  fi
 
-    if ! type -p brew > /dev/null; then
-        message_warning "${NVIM_MESSAGE_BREW}"
-        return 0
-    fi
+  if ! type -p brew > /dev/null; then
+      message_warning "${NVIM_MESSAGE_BREW}"
+      return 0
+  fi
 
-    hash git >/dev/null 2>&1 || {
-        brew install git
-    }
+  hash git >/dev/null 2>&1 || {
+      brew install git
+  }
 
-    hash curl >/dev/null 2>&1 || {
-        brew install curl
-    }
+  hash curl >/dev/null 2>&1 || {
+      brew install curl
+  }
 
-    hash wget >/dev/null 2>&1 || {
-        brew install wget
-    }
-    message_success "Installed dependences ${NVIM_PACKAGE_NAME}"
+  hash wget >/dev/null 2>&1 || {
+      brew install wget
+  }
+  message_success "Installed dependences ${NVIM_PACKAGE_NAME}"
+}
+
+# nvim::backup::config - backup configuration NVIM app
+nvim::backup::config() {
+  message_info "You will see your old ${NVIM_DST_NVIM_DIR} as ${NVIM_DST_NVIM_DIR}/NVIM.bak"
+  mv "${NVIM_DST_NVIM_DIR}" "${NVIM_DST_NVIM_DIR}/NVIM.bak"
+  message_success "backup config ${NVIM_PACKAGE_NAME}"
 }
 
 # nvim::install - install NVIM app
 nvim::install() {
-    nvim::install::dependences
-    if ! type -p brew > /dev/null; then
-        message_warning "${NVIM_MESSAGE_BREW}"
-        return
-    fi
-    if ! type -p nvim > /dev/null; then
-        message_info "Install ${NVIM_PACKAGE_NAME}"
-        brew install nvim
-        message_success "Installed ${NVIM_PACKAGE_NAME}"
-    fi
+  nvim::install::dependences
+  if ! type -p brew > /dev/null; then
+      message_warning "${NVIM_MESSAGE_BREW}"
+      return
+  fi
+  if ! type -p nvim > /dev/null; then
+      message_info "Install ${NVIM_PACKAGE_NAME}"
+      brew install nvim
+      message_success "Installed ${NVIM_PACKAGE_NAME}"
+  fi
+}
+
+nvim::clean() {
+  message_info "start clean configurations ${NVIM_PACKAGE_NAME}"
+  rm -rf ~/.config/nvim
+  rm -rf ~/.local/share/nvim
+  rm -rf ~/.cache/nvim
+  message_success "finish clean configurations ${NVIM_PACKAGE_NAME}"
+}
+
+nvchad::install() {
+  message_info "Install nvchad"
+  git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
+  message_success "Installed nvchad"
 }
 
 nvim::post_install() {
-    message_info "Post Install ${NVIM_PACKAGE_NAME}"
+  message_info "Post Install ${NVIM_PACKAGE_NAME}"
 
-    message_info "Looking for an existing NVIM config..."
-    if [ -d "${NVIM_DST_NVIM_DIR}" ]; then
-        message_warning "Found ${NVIM_DST_NVIM_DIR}"
-        message_info "You will see your old ${NVIM_DST_NVIM_DIR} as ${NVIM_DST_NVIM_DIR}/NVIM.bak"
-        mv "${NVIM_DST_NVIM_DIR}" "${NVIM_DST_NVIM_DIR}/NVIM.bak"
-    fi
+  message_info "Looking for an existing NVIM config..."
+  if [ -d "${NVIM_DST_NVIM_DIR}" ]; then
+      message_warning "Found ${NVIM_DST_NVIM_DIR}"
+      nvim::backup::config
+  fi
 
-    # Prevent the cloned repository from having insecure permissions. Failing to do
-    # so causes compinit() calls to fail with "command not found: compdef" errors
-    # for users with insecure umasks (e.g., "002", allowing group writability). Note
-    # that this will be ignored under Cygwin by default, as Windows ACLs take
-    # precedence over umasks except for filesystems mounted with option "noacl".
-    umask g-w,o-w
+  nvchad::install
 
-    message_info "Cloning NVIM from ${NVIM_REPO_HTTPS}"
+  # Prevent the cloned repository from having insecure permissions. Failing to do
+  # so causes compinit() calls to fail with "command not found: compdef" errors
+  # for users with insecure umasks (e.g., "002", allowing group writability). Note
+  # that this will be ignored under Cygwin by default, as Windows ACLs take
+  # precedence over umasks except for filesystems mounted with option "noacl".
+  umask g-w,o-w
 
-    env git clone --depth=1 "${NVIM_REPO_HTTPS}" --branch develop "${NVIM_ROOT_PATH}" || {
-        message_warning "git clone of NVIM repo failed."
-        return
-    }
+  message_info "Cloning NVIM from ${NVIM_REPO_HTTPS}"
 
-    nvim::install::vimplug
+  env git clone --depth=1 "${NVIM_REPO_HTTPS}" --branch main "${NVIM_ROOT_PATH}" || {
+      message_warning "git clone of NVIM repo failed."
+      return
+  }
 
-    ln -fs "${NVIM_ROOT_PATH}/nvim" "${NVIM_DST_NVIM_DIR}"
+  ln -fs "${NVIM_ROOT_CUSTOM_PATH}" "${NVIM_DST_CUSTOM_DIR}"
 
-    message_success "${NVIM_MESSAGE_DONE}"
+  message_success "${NVIM_MESSAGE_DONE}"
 
-    # Install plugins using vim-plug
-    nvim +PlugInstall +PlugUpdate +PlugClean! +qall
-    message_success "Post Installed ${NVIM_PACKAGE_NAME}"
+  nvim +'hi NormalFloat guibg=#1e222a' +PackerSync
+  message_success "Post Installed ${NVIM_PACKAGE_NAME}"
 }
 
 nvim::install
