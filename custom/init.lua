1c97a999d5f5de0e1a9ee4703625cab5368756fc
Luis Mayta <luis@hadenlabs.com>
2022-09-04 13:52:37 -0300

    refactor: change configuration mapleader (#70)

--- custom/init.lua
+++ custom/init.lua
@@ -1,8 +1,6 @@
-vim.opt.foldmethod="indent"
-vim.opt.foldlevelstart=99
-vim.opt.swapfile=false
-vim.g.mapleader = ","
-vim.opt.tabstop = 2
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevelstart = 99
+vim.opt.swapfile = false
 
 vim.cmd("command W SudaWrite")
 nvchad.no_WhichKey_map()
